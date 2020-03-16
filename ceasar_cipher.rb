aff01a69cff7426c14a25cc68eb3cc1e092f7e3f
Cinthia Godoi <cinthiagodoi@hotmail.com>
2020-03-16 18:33:37 +0000

    Renamed File

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,10 +1,12 @@
 print "What's your text? \n"
 str = gets.chomp.downcase
+
 def ceaser_cipher(str)
   a = ("a".."z").to_a
   str.split("").map{|c| next c if c == ' '; a.rotate(a.find_index(c) + 5).first}.join('')
 end
-p ceaser_cipher(str)
+
+print ceaser_cipher(str)
 
 
   
